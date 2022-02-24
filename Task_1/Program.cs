f71bec936303d2d2fc685eba90d4156a6521f30e
Evgeniy <jon83@inbox.ru>
2022-02-25 00:17:40 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2. Программа сравнения 2-х чисел
+Console.Write("Введите первое число ");
+int FirstNumber = Convert.ToInt16(Console.ReadLine());
+Console.Write("Введите второе число ");
+int SecondNumber = Convert.ToInt16(Console.ReadLine());
+if (FirstNumber > SecondNumber)
+{
+    Console.WriteLine($"Max число равно {FirstNumber}, а минимальное  равно {SecondNumber}");
+}
+else
+{
+Console.WriteLine($"Max число равно {SecondNumber}, а минимальное равно {FirstNumber}");
+}
