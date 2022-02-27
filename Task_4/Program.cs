30ae0107117f105381a0331f0bc9afd1ff55f9be
Evgeniy <jon83@inbox.ru>
2022-02-27 22:22:11 +0300

    Task_4

---
+++ Task_4/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа принимающаю число (N) и выводящая все четные числа от 1 до (N)
+Console.Write("Введите число: ");
+int N = Convert.ToInt16(Console.ReadLine());
+int EvenNumber = 2;
+while (EvenNumber <= N)
+{
+    Console.Write($"{EvenNumber}  ");
+    EvenNumber +=2;
+}
+//Вариант №2 решение
+// int count = 1;
+// while (count <= N)
+// {
+//     if (count % 2 == 0)
+//     {
+//         Console.Write($"{count}  ");
+//         count++;
+//     }
+//     else
+//     {
+//         count++;
+//     }
+// }
