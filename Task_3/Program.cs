ede2ae885e1acc4c787e9ea1b1ff089cc2bdfefd
Evgeniy <jon83@inbox.ru>
2022-02-27 22:02:38 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверка числа на четность
+Console.Write("Введите число: ");
+int number = Convert.ToInt16(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.Write($"Число {number} четное");
+}
+else
+{
+    Console.Write($"Число {number} нечетное");
+}
